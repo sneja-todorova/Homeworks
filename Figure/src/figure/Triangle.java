3305d3675e9ac6e10ab31504c4261b9d97480355
sneja-todorova <sneji_28@abv.bg>
2013-11-22 01:37:41 +0200

    Update Triangle.java

--- Figure/src/figure/Triangle.java
+++ Figure/src/figure/Triangle.java
@@ -3,15 +3,11 @@ package figure;
 public class Triangle extends PlainFigure{
 	public Triangle(double a1, double a2, double a3) throws TriangleException{
 		super(a1, a2, a3);
-		if ( a1+a2<a3 && a2+a3<a1 && a1+a3<a2 && a1<0 && a2<0 && a3<0){
+		if ( a1+a2<a3 || a2+a3<a1 || a1+a3<a2 || a1<0 || a2<0 || a3<0){
 			throw new TriangleException();
 		}
 	}
 
-	@Override
-	public boolean equals(Figure x) {
-		return false;
-	}
 
 	@Override
 	public double getArea() {
